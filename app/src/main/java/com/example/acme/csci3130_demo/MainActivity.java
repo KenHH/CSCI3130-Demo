3d9fb30a080f9443acb84e0bbf76e8c137e65bf0
KenHH <kennth_5@hotmail.com>
2018-01-23 00:00:20 -0400

    FIX A1

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button submit = findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            EditText txt;
+            TextView output;
+            public void onClick(View view) {
+                txt = findViewById(R.id.editText);
+                output = findViewById(R.id.textView);
+                output.setText(txt.getText());
+            }
+        });
+
+
     }
 }
